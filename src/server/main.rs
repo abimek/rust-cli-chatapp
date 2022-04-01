6283a97a3a69746a2125c7730200f657ca276c05
abimekuriya <abi.mek2005@gmail.com>
2022-03-31 21:59:40 -0400

    Initial Commit

---
+++ src/server/main.rs
@@ -0,0 +1,8 @@
+use std::net::TcpStream;
+
+
+fn main(){
+
+    
+
+}
---
+++ src/server/user.rs
@@ -0,0 +1,14 @@
+pub mod User {
+    
+    struct User {
+        username: String    
+    }
+
+
+impl User {
+
+    fn new(name: String){
+
+    }
+    
+}
