30cb81bffff5174192dc8985a183874ef0a793df
abimekuriya <abi.mek2005@gmail.com>
2022-04-01 20:05:28 -0400

    Base work on Channels, Server, and User complete

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -1,43 +1,84 @@
-
 use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::collections::HashMap;
 
 //USER CODE
 use std::net::{TcpStream}
 
-struct User {
+struct User<String> {
+    identifier: i32,
     username: String,
-    stream: TcpStream
+    stream: TcpStream,
+    channels: Vec<String>
 }
-
 
 impl User {
 
-    fn new(stream: TcpStream) -> Self {
-
-    }        
-}
+    fn new(id: i32, tcp_stream: TcpStream) -> Self {
+        //TODO Read the Username from Stream
+        User{
+            identifier: id,
+            username, 
+            stream: tcp_stream,
+            channels: Vec::new();
+        }
+    }
     
+    async fn handle_packets(&self){
+        loop {
 
+        }
+    }
+}
+//Channel Code
+
+struct Channel {
+    identifier: String,
+    users: Vec<i32>
+}
+
+impl Channel {
+
+    fn new(identifier: String) -> Self {
+        Channel{
+            identifier,
+            users: Vec::new()
+        }
+    }
+
+    fn add_user(identifier: i32){
+        users.push(identifier);
+    }
+
+}
+
+    
 //SERVER CODE
 struct Server {
     users: Vec<User>,
-    channels: Vec<User>
+    channels: HashMap<String, Channel>,
+    usercount: i32
 }
 
 impl Server {
     fn new() -> Self {
         Server {
-            users: Vec<User>::new(),
-            channels: Vec<Channel>::new()       
+            users: Vec::new(),
+            channels: HashMap::new(),
+            usercount: 0       
         }
+    }
+
+    fn addChannel(&self, channel: Channel){
+        self.channels.insert(channel.identifier, channel);
+    }
+
+    fn increment_user_count(&self) -> i32 {
+        self.usercount++;
+        self.usercount.clone();
     }
 
     fn addUser(&self, user: User){
         self.users.push(user);
-    }
-
-    fn addChannel(&self, channel: Channel){
-        self.channels.push(channel);
     }
 }
 
@@ -49,11 +90,15 @@ async fn main(){
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        handle_client(tcp_stream, server);
+        let userid = server.increment_user_count();
+        handle_client(tcp_stream, server, userid);
     }
 
 }
 
-async fn handle_client(stream: TcpStream, server: &mut Server) {
-    server.addUser()
+async fn handle_client(stream: TcpStream, server: &mut Server, userid: i32) {
+    let mut user = User::new(userid, stream);
+    user.handle_packets();
+    server.addUser(user);
 }
+
