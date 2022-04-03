3c4c953696cdd27daa7592c8e359e1f0a32f9e0f
abimekuriya <abi.mek2005@gmail.com>
2022-04-02 21:20:25 -0400

    Refractoring packets so that this application is less complicated

--- src/packets/lib.rs
+++ src/packets/lib.rs
@@ -1,84 +1,7 @@
 use serde::{Serialize, Deserialize};
-use serde_json::{Result, Value};
-
-trait Packet {
-    fn get_id() -> i32;
-}
-
-#[derive(Serialize, Deserialize)]
-struct RequestLoginPacket {
-    username: String
-}
-
-impl Packet for RequestLoginPacket{
-    fn get_id() -> i32 {
-        0
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct ValidateLoginPacket {
-    valid: bool
-}
-
-impl Packet for ValidateLoginPacket{
-    fn get_id() -> i32 {
-        1
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct RequestJoinChannelPacket {
-    channel_name: String,
-    channel_password: Option<String>
-}
-
-impl Packet for RequestJoinChannelPacket{
-    fn get_id() -> i32 {
-        2
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-enum ChannelJoinFailure {
-    IncorrectPassword
-}
-
-
-#[derive(Serialize, Deserialize)]
-struct ConfirmChannelJoinPacket {
-    valid: bool,
-    reason: Option<ChannelJoinFailure>
-}
-
-impl Packet for ConfirmChannelJoinPacket{
-    fn get_id() -> i32 {
-        2
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct RequestLeaveChannelPacket {
-    channel_name: String,
-    channel_password: Option<String>
-}
-
-impl Packet for RequestLeaveChannelPacket{
-    fn get_id() -> i32 {
-        3
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct ConfirmChannelLeavePacket {
-    valid: bool
-}
-
-impl Packet for ConfirmChannelLeavePacket{
-    fn get_id() -> i32 {
-        4
-    }
-}
+use serde_json::from_str;
+use serde_json::to_string;
+use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
 struct MessageSendPacket {
@@ -86,12 +9,16 @@ struct MessageSendPacket {
     message: String
 }
 
-impl Packet for MessageSendPacket{
-    fn get_id() -> i32 {
-        5
+impl MessageSendPacket {
+
+    fn from_string(data: &str) -> Result<String> {
+        from_str(data)
+    }
+
+    fn to_string(&self) -> Result<String> {
+        to_string(&self)
     }
 }
-
 
 
 
