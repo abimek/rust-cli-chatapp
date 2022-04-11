6247678c128ea146c356e4f55501397711eb1bfc
abimekuriya <abi.mek2005@gmail.com>
2022-04-11 17:25:03 -0400

    Implemented Reading Packets from Stream

--- src/packets/lib.rs
+++ src/packets/lib.rs
@@ -1,22 +1,75 @@
 use serde::{Deserialize, Serialize};
-use tokio::net::TcpStream;
+use tokio::{io::{BufWriter, BufReader, AsyncBufReadExt, AsyncReadExt}, net::{TcpStream, tcp::{OwnedWriteHalf, OwnedReadHalf}}};
 
 type Result<T> = std::result::Result<T, Error>;
 
 enum Error {
-    ReadLineError,
+    PacketError(PacketErrorType),
+    ReadDataError(ReadDataErrorType),
+}
+
+enum ReadDataErrorType {
+    FailedPacketIdRead
+}
+
+enum PacketErrorType {
+     NoneExistantPacketId,
+     PacketConstructionFail
 }
 
 pub struct Connection {
-    stream: TcpStream,
+    writer: BufWriter<OwnedWriteHalf>,
+    reader: BufReader<OwnedReadHalf>
 }
 
 impl Connection {
     pub fn new(stream: TcpStream) -> Self {
-        Connection { stream }
+
+        let (mut read, mut write) = stream.into_split();
+        Connection {
+            writer: BufWriter<write>,
+            reader: BufReader<read>
+        }
     }
 
-    pub fn read_data() -> Result<Box<dyn Packet>> {}
+    async fn read_data(&mut self) -> Result<Box<dyn Packet>> {
+        match self.reader.read_u64().await {
+            Ok(id) => {
+                match PacketId::from_u64(id) {
+                    Some(packet) => {
+                        let mut data = String::new();
+                        self.reader.read_line(&mut data);
+                        match packet {
+                            PacketId::MessageSend => {
+                                if let Ok(pk) = MessageSendPacket::from_string(&data) {
+                                    return Ok(Box::new(pk))
+                                }
+                                return Err(Error::PacketError(PacketErrorType::PacketConstructionFail));
+                            },
+                            _ => Err(Error::PacketError(PacketErrorType::NoneExistantPacketId))
+                        } 
+                    },
+                    None => return Err(Error::PacketError(PacketErrorType::NoneExistantPacketId))
+                }
+            },
+            std::result::Result::Err(n) => {
+                return Err(Error::ReadDataError(ReadDataErrorType::FailedPacketIdRead));
+            } 
+        }
+    }
+}
+
+pub enum PacketId {
+    MessageSend
+}
+
+impl PacketId {
+   pub fn from_u64(int: u64) -> Option<PacketId> {
+        match int {
+            0 => Some(PacketId::MessageSend),
+            _ => None
+        }
+   } 
 }
 
 pub trait Packet {
@@ -25,6 +78,10 @@ pub trait Packet {
         Self: Sized;
 
     fn to_string(&self) -> serde_json::Result<String>
+    where
+        Self: Sized;
+
+    fn get_id() -> PacketId
     where
         Self: Sized;
 }
@@ -43,5 +100,9 @@ impl Packet for MessageSendPacket {
 
     fn to_string(&self) -> serde_json::Result<String> {
         serde_json::to_string(&self)
+    }
+
+    fn get_id() -> PacketId {
+        PacketId::MessageSend
     }
 }
